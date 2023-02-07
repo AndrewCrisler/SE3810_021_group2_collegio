fa658c3ec876cf367c297fc53ef74e983d9bebd8
AndrewCrisler <andrew.t.c000@gmail.com>
2023-02-07 08:27:54 -0600

    Added home, search, clubs, and about us page and completed header

--- start.js
+++ start.js
@@ -8,23 +8,12 @@ const port = 3002;
 
 // app.set('views', path.join(__dirname, 'frontend/views/public_views')); //set the view path
 // app.set('view engine', 'ejs'); //allows ejs usage
-app.use(express.static(path.join(__dirname, 'frontend/views/public_views/')));
+app.use(express.static(path.join(__dirname, 'frontend/views/msoe_views/')));
 app.use(express.static(path.join(__dirname, 'frontend/assets')));
 app.use(helmet());
 
 app.get('/', async (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/views/public_views/home.html'));
-});
-
-let is_msoe_subscribed = false;
-app.post('/subscribe_college', async (req, res) => {
-  if(!is_msoe_subscribed) {
-    app.get('/msoe/', async (req, res) => {
-      res.sendFile(path.join(__dirname, 'frontend/views/msoe_views/home.html'));
-    });
-    app.use('/msoe', express.static(path.join(__dirname, 'frontend/views/msoe_views/')));
-    res.sendFile(path.join(__dirname, 'frontend/views/msoe_views/home.html'));
-  }
+  res.sendFile(path.join(__dirname, 'frontend/views/msoe_views/home.html'));
 });
 
 app.listen(port, () => {
