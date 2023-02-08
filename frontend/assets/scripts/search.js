82d16aa5a5994e6a1efbe9d33d7b19d245def92d
Desai <desain@msoe.edu>
2023-02-08 14:42:08 -0600

    fake clubs

--- frontend/assets/scripts/search.js
+++ frontend/assets/scripts/search.js
@@ -18,7 +18,22 @@ const clubButtons = [
     name: 'robotics club',
     desc: 'skynet will destroy us',
     html: '<a href="/clubs/robotics_home.html"><div class="club"><img src="/clubs/robotics.png"><div class="desc"><h2>Robotics</h2><span>Desc of Robotics ...</span></div></div></a>'
-  }
+  },
+    {
+    name: 'Ping Pong Club',
+    desc: 'The club for people who love to play ping pong! Both competitive and casual players are welcome!',
+    html: '<a href="/clubs/fakeclub1.html"><div class="club"><img src="/clubs/fakeclub1.jpg"><div class="desc"><h2>Ping Pong Club</h2><span>The club for people who love to play ping pong! Both competitive and casual players are welcome!</span></div></div></a>'
+  },
+  {
+    name: 'Bowling Club',
+    desc: 'The club for people who love to bowl! Both competitive and casual players are welcome!',
+    html: '<a href="/clubs/fakeclub2.html"><div class="club"><img src="/clubs/fakeclub2.jpg"><div class="desc"><h2>Bowling Club</h2><span>The club for people who love to bowl! Both competitive and casual players are welcome!</span></div></div></a>'
+  },
+  {
+    name: 'Billiards Club',
+    desc: 'The club for people who love to play billiards! Both competitive and casual players are welcome!',
+    html: '<a href="/clubs/fakeclub3.html"><div class="club"><img src="/clubs/fakeclub3/fakeclub3.jpg"><div class="desc"><h2>Billiards Club</h2><span>The club for people who love to play billiards! Both competitive and casual players are welcome!</span></div></div></a>'
+  },
 ];
 
 const defaultResponse = '<h3 class="infoText">Start typing letters to search</h3>';
